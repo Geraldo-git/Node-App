94f063716a742dd9b7dc55bda5aed58c68976b34
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-28 07:51:24 -0300

    Primeiro endpoint e CORS

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,31 @@
-console.log('Funcionamdo')
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+
+const app = express();
+app.use(cors);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello people!</h1>");
+});
+
+app.get("/alunos", (req, res) => {
+  res.json([
+    {
+      aluno: "Bia",
+      idade: 13,
+      classe: "Oitavo ano",
+    },
+    {
+      aluno: "Gui",
+      idade: 14,
+      classe: "Nono ano",
+    },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("Listen on port 3000...");
+});
---
+++ src/users/test.ts
@@ -0,0 +1,1 @@
+import { AppServer } from "@shared/http/server";
